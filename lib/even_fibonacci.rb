bc61076b281ec124271234cb80132946067156ac
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-29 23:24:39 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
-# Implement your procedural solution here!
+def fibonacci(limit)
+  x=1
+  y=0
+  fibarray= Array.new
+    until x+y>=limit
+      num=x+y
+      fibarray<<num
+      y=x
+      x=num
+    end
+    fibarray.select {|i| i%2==0}
+end
+
+def even_fibonacci_sum(limit)
+  sum=0
+  fibonacci(limit).each {|a| sum+=a}
+  return sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,32 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+def initialize(limit)
+@limit=limit
+end
+
+def limit=(limit)
+end
+
+def limit
+  @limit
+end
+
+def fibonacci
+   x=1
+  y=0
+  fibarray= Array.new
+    until x+y>=@limit
+      num=x+y
+      fibarray<<num
+      y=x
+      x=num
+    end
+    fibarray.select {|i| i%2==0}
+end
+def sum
+  sum=0
+  fibonacci.each {|a| sum+=a}
+  return sum
+end
+
+
+end
